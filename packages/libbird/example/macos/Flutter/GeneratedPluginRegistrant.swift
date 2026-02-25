d5b0288eb6d64a745f5687e5463deb3c1564776a
Eric Apostal <ehapostal04@gmail.com>
2026-02-25 17:53:38 -0500

    remake example

---
+++ packages/libbird/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import libbird
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LibbirdPlugin.register(with: registry.registrar(forPlugin: "LibbirdPlugin"))
+}
