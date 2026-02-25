209a5d3f4fdbab4543c6888ba383f1ae1d4d7c4d
Eric Apostal <ehapostal04@gmail.com>
2026-02-25 17:46:33 -0500

    revert stuff

--- packages/libbird/macos/libbird/Sources/libbird/HostedPlatformViewFactory.swift
+++ packages/libbird/macos/libbird/Sources/libbird/HostedPlatformViewFactory.swift
@@ -1,5 +1,12 @@
+//
+//  HostedPlatformView.swift
+//  libbird
+//
+//  Created by Eric Apostal on 2/24/26.
+//
+
 import FlutterMacOS
-import Foundation
+import AppKit
 
 class HostedPlatformViewFactory: NSObject, FlutterPlatformViewFactory {
     private var messenger: FlutterBinaryMessenger
@@ -9,20 +16,20 @@ class HostedPlatformViewFactory: NSObject, FlutterPlatformViewFactory {
         super.init()
     }
 
-    func create(
-        withViewIdentifier viewId: Int64,
-        arguments args: Any?
-    ) -> NSView {
-        let wrapper = LadybirdViewWrapper()
-        if let argsDict = args as? [String: Any], let url = argsDict["url"] as? String {
-            wrapper.loadURL(url)
-        } else {
-            wrapper.loadURL("https://ladybird.org")
-        }
-        return wrapper.getView()
-    }
-    
-    func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
-        return FlutterStandardMessageCodec.sharedInstance()
+    func create(withViewIdentifier viewId: Int64, arguments args: Any?) -> NSView {
+        let view = NSView(frame: NSRect(x: 0, y: 0, width: 200, height: 200))
+        
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.systemBlue.cgColor
+        
+        let label = NSTextField(labelWithString: "Native macOS View")
+        label.isEditable = false
+        label.isBordered = false
+        label.backgroundColor = NSColor.clear
+        label.frame = NSRect(x: 10, y: 10, width: 150, height: 30)
+        
+        view.addSubview(label)
+        
+        return view
     }
 }
