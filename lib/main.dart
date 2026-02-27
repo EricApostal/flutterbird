ef06c4a560e482fe6345d27d2ee12f0a1e454dde
Eric Apostal <ehapostal04@gmail.com>
2026-02-26 23:01:46 -0500

    move stuff to libbird

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterbird/native_view.dart';
+import 'package:libbird/native_view.dart';
 
 void main() {
   runApp(const MainApp());
--- packages/libbird/lib/libbird.dart
+++ packages/libbird/lib/libbird.dart
@@ -1,5 +1,5 @@
-
 import 'libbird_platform_interface.dart';
+export 'native_view.dart';
 
 class Libbird {
   Future<String?> getPlatformVersion() {
