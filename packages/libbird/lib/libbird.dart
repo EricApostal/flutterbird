8248db449180feacea0c78224533cdc89a5c74c1
Eric Apostal <ehapostal04@gmail.com>
2026-03-01 20:15:28 -0500

    start moving over to controller

--- packages/libbird/lib/libbird.dart
+++ packages/libbird/lib/libbird.dart
@@ -1,8 +1,7 @@
-import 'libbird_platform_interface.dart';
 export 'native_view.dart';
 
-class Libbird {
-  Future<String?> getPlatformVersion() {
-    return LibbirdPlatform.instance.getPlatformVersion();
-  }
-}
+// class Libbird {
+//   Future<String?> getPlatformVersion() {
+//     return LibbirdPlatform.instance.getPlatformVersion();
+//   }
+// }
