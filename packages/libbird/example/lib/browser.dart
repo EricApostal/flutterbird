c8dc79149db52a232c05b3e0833d5faa45f274a2
Eric Apostal <ehapostal04@gmail.com>
2026-03-05 21:54:34 -0500

    basic browser example

---
+++ packages/libbird/example/lib/browser.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:libbird/libbird.dart';
+
+class BrowserWindow extends StatefulWidget {
+  const BrowserWindow({super.key});
+
+  @override
+  State<BrowserWindow> createState() => _BrowserWindowState();
+}
+
+class _BrowserWindowState extends State<BrowserWindow> {
+  final _controller = LadybirdController();
+  final _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final radius = 12.0;
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _textController,
+              onSubmitted: (value) {
+                _controller.navigate(value);
+              },
+              scrollPadding: .all(0),
+              decoration: InputDecoration(
+                hintText: "Search",
+                filled: true,
+                fillColor: theme.colorScheme.surfaceContainerLow,
+                hintStyle: theme.textTheme.labelLarge!.copyWith(
+                  color: theme.colorScheme.surfaceContainerHighest,
+                  fontWeight: .w400,
+                  fontSize: 13.5,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(radius),
+                  borderSide: BorderSide(
+                    color: theme.colorScheme.surfaceContainerHigh,
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(radius),
+                  borderSide: BorderSide(
+                    color: theme.colorScheme.primary,
+                    width: 1,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(radius),
+                  borderSide: BorderSide(
+                    color: theme.colorScheme.error,
+                    width: 1,
+                  ),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(radius),
+                  borderSide: BorderSide(
+                    color: theme.colorScheme.error,
+                    width: 1,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: LadybirdView(controller: _controller)),
+        ],
+      ),
+    );
+  }
+}
--- packages/libbird/example/lib/main.dart
+++ packages/libbird/example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:libbird/libbird.dart';
+import 'package:libbird_example/browser.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,24 +13,18 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  final _controller = LadybirdController();
-  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            TextField(controller: _textController),
-            Expanded(child: LadybirdView(controller: _controller)),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _controller.navigate(_textController.text);
-          },
+      themeMode: .dark,
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 18, 34, 127),
+          brightness: .dark,
         ),
       ),
+      theme: ThemeData.dark(),
+      home: BrowserWindow(),
     );
   }
 }
