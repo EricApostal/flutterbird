4833d55f5fa4bf67a3a1d60e4b6fc25975a5e36b
Eric Apostal <ehapostal04@gmail.com>
2026-03-06 21:44:01 -0500

    view id private

--- packages/libbird/lib/src/controller.dart
+++ packages/libbird/lib/src/controller.dart
@@ -10,7 +10,7 @@ class LadybirdController {
   late final LadybirdBindings _bindings;
   // int? _textureId;
   Size? _lastSize;
-  late final int viewId;
+  late final int _viewId;
 
   late final ffi.NativeCallable<ffi.Void Function()> _resizeCallback;
   void Function()? onResize;
@@ -20,12 +20,12 @@ class LadybirdController {
     _bindings = LadybirdBindings(_lib);
 
     _bindings.init_ladybird();
-    viewId = _bindings.create_web_view();
+    _viewId = _bindings.create_web_view();
 
     _resizeCallback = ffi.NativeCallable<ffi.Void Function()>.listener(
       _onResize,
     );
-    _bindings.set_resize_callback(viewId, _resizeCallback.nativeFunction);
+    _bindings.set_resize_callback(_viewId, _resizeCallback.nativeFunction);
   }
 
   void _onResize() {
@@ -34,12 +34,12 @@ class LadybirdController {
 
   void navigate(String url) {
     final ffi.Pointer<Utf8> charPointer = url.toNativeUtf8();
-    _bindings.navigate_to(viewId, charPointer.cast<ffi.Char>());
+    _bindings.navigate_to(_viewId, charPointer.cast<ffi.Char>());
     malloc.free(charPointer);
   }
 
   Future<int> createTexture() async {
-    return await _channel.invokeMethod('createTexture', viewId);
+    return await _channel.invokeMethod('createTexture', _viewId);
   }
 
   Future<void> unregisterTexture(int textureId) async {
@@ -49,16 +49,16 @@ class LadybirdController {
   bool resizeWindow(Size size) {
     if (size == _lastSize) return false;
     _lastSize = size;
-    _bindings.resize_window(viewId, size.width.toInt(), size.height.toInt());
+    _bindings.resize_window(_viewId, size.width.toInt(), size.height.toInt());
     return true;
   }
 
   int getSurfaceWidth() {
-    return _bindings.get_iosurface_width(viewId);
+    return _bindings.get_iosurface_width(_viewId);
   }
 
   int getSurfaceHeight() {
-    return _bindings.get_iosurface_height(viewId);
+    return _bindings.get_iosurface_height(_viewId);
   }
 
   void dispatchMouseEvent({
@@ -72,7 +72,7 @@ class LadybirdController {
     required int wheelDeltaY,
   }) {
     _bindings.dispatch_mouse_event(
-      viewId,
+      _viewId,
       type,
       x,
       y,
@@ -92,7 +92,7 @@ class LadybirdController {
     required bool repeat,
   }) {
     _bindings.dispatch_key_event(
-      viewId,
+      _viewId,
       type,
       keycode,
       modifiers,
@@ -103,6 +103,6 @@ class LadybirdController {
 
   void dispose() {
     _resizeCallback.close();
-    _bindings.destroy_web_view(viewId);
+    _bindings.destroy_web_view(_viewId);
   }
 }
