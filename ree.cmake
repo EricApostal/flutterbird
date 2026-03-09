a0454641c5c109fbb6603a9df206e734c48c31ef
Eric Apostal <ehapostal04@gmail.com>
2026-03-08 22:30:26 -0400

    cmake ver

--- ree.cmake
+++ ree.cmake
@@ -11,7 +11,7 @@ cd /Users/eric/Documents/development/projects/flutterbird/packages/libbird/andro
   -DANDROID_NDK=/Users/eric/Library/Android/sdk/ndk/27.0.12077973 \
   -DCMAKE_ANDROID_NDK=/Users/eric/Library/Android/sdk/ndk/27.0.12077973 \
   -DCMAKE_TOOLCHAIN_FILE=/Users/eric/Library/Android/sdk/ndk/27.0.12077973/build/cmake/android.toolchain.cmake \
-  -DCMAKE_MAKE_PROGRAM=/Users/eric/Library/Android/sdk/cmake/3.22.1/bin/ninja \
+  -DCMAKE_MAKE_PROGRAM=/Users/eric/Library/Android/sdk/cmake/4.1.2/bin/ninja \
   "-DCMAKE_CXX_FLAGS=-std=c++2b -frtti -fexceptions -D__ANDROID_API__=30 -D__GCC_DESTRUCTIVE_SIZE=64 -Wno-invalid-constexpr" \
   -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/Users/eric/Documents/development/projects/flutterbird/build/ladybird/intermediates/cxx/RelWithDebInfo/656l5o1b/obj/arm64-v8a \
   -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=/Users/eric/Documents/development/projects/flutterbird/build/ladybird/intermediates/cxx/RelWithDebInfo/656l5o1b/obj/arm64-v8a \
