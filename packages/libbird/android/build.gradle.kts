a8c25d842f57efb95d2ac8a5f5869a0897bf3d26
Eric Apostal <ehapostal04@gmail.com>
2026-03-08 23:58:31 -0400

    move to gradle

--- packages/libbird/android/build.gradle.kts
+++ packages/libbird/android/build.gradle.kts
@@ -1,6 +1,11 @@
 group = "com.example.ladybird"
 version = "1.0-SNAPSHOT"
 
+var buildDir = layout.buildDirectory.get()
+var cacheDir = System.getenv("LADYBIRD_CACHE_DIR") ?: "$buildDir/caches"
+var sourceDir = layout.projectDirectory.dir("../third_party/ladybird").toString()
+var ladybirdAndroidDir = layout.projectDirectory.dir("../third_party/ladybird/UI/Android").toString()
+
 buildscript {
     repositories {
         google()
@@ -22,16 +27,39 @@ plugins {
     id("com.android.library")
 }
 
+val buildLagomTools = tasks.register<Exec>("buildLagomTools") {
+    workingDir = file(ladybirdAndroidDir)
+    commandLine = listOf("./BuildLagomTools.sh")
+    environment = mapOf(
+        "BUILD_DIR" to buildDir,
+        "CACHE_DIR" to cacheDir,
+        "PATH" to System.getenv("PATH")!!
+    )
+}
+tasks.named("preBuild") {
+    dependsOn(buildLagomTools)
+}
+tasks.matching { it.name == "prepareKotlinBuildScriptModel" }.configureEach {
+    dependsOn(buildLagomTools)
+}
+
 android {
     namespace = "com.example.ladybird"
     compileSdk = 35
+    ndkVersion = "29.0.13599879"
 
     defaultConfig {
         minSdk = 30
         externalNativeBuild {
             cmake {
                 cppFlags += "-std=c++2b -frtti -fexceptions -D__ANDROID_API__=30 -D__GCC_DESTRUCTIVE_SIZE=64 -Wno-invalid-constexpr"
-                arguments += listOf("-DANDROID_STL=c++_shared")
+                arguments += listOf(
+                    "-DLagomTools_DIR=$buildDir/lagom-tools-install/share/LagomTools",
+                    "-DANDROID_STL=c++_shared",
+                    "-DLADYBIRD_CACHE_DIR=$cacheDir",
+                    "-DVCPKG_ROOT=$sourceDir/Build/vcpkg",
+                    "-DVCPKG_TARGET_ANDROID=ON"
+                )
             }
         }
         ndk {
